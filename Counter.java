07358e210346fbfb75ada531073c874abf9582af
Jasmine120207 <jasu67524@gmail.com>
2025-08-20 11:54:07 +0530

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,14 @@
+class Static{
+      static int count=0;
+      Static(){
+          count++;
+          System.out.println("count :" + count);
+      }
+}
+public class Counter{
+      public static void main(String[] args){
+           Static c1=new Static();
+           Static c2=new Static();
+           Static c3=new Static();
+      }
+}
